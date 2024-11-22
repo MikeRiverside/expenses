077a411ef14513fa71c211fb678501aa634a00c1
Mihkel Jõesalu <mihkel.joesalu@voco.ee>
2024-11-22 11:03:59 +0200

    added date component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,13 +1,19 @@
+import ExpenseDate from "./ExpenseDate"
 import "./ExpenseItem.css"
 
-const ExpenseItem = () => {
-    return (<div className="expense-item">
-        <div>Date</div>
-        <div className="expense-item_description">
-            <h2>Title</h2>
-            <div className="expense-item_price">Price</div>
-        </div>
-    </div>)
+const ExpenseItem = (props) => {
+
+    console.log(props.data.date)
+    
+
+    return (
+        <div className="expense-item">
+            <ExpenseDate date={props.data.date}/>
+            <div className="expense-item_description">
+                <h2>{props.data.title}</h2>
+                <div className="expense-item_price">{props.data.price}</div>
+            </div>
+         </div>)
 }
 
 export default ExpenseItem
