8a63e2df2631eefcc84b901ae763064fdade432c
Mihkel Jõesalu <mihkel.joesalu@voco.ee>
2025-01-10 11:33:38 +0200

    valikuline kuvamine

--- src/components/Expenses/ExpensesList.js
+++ src/components/Expenses/ExpensesList.js
@@ -2,17 +2,17 @@ import "./ExpensesList.css";
 import ExpenseItem from "./ExpenseItem";
 
 const ExpensesList = (props) => {
+    if(props.expenses.length === 0){  
+        return <h2 className="expenses-list__fallback">No expenses found!</h2>
+    }   
     return(
-        <>
-                {
-                props.expenses.length === 0 && <h2>No expenses found!</h2>
-                }
-            <ul>
-            {props.expenses.length > 0 && props.expenses.map((expense) => (
-                <ExpenseItem key={expense.id} data={expense} />
-            ))}
-            </ul>
-        </>
+        <ul className="expenses-list">
+            {props.expenses.length > 0 && props.expenses.map((expense) => {
+                return <ExpenseItem key={expense.id} data={expense} />;
+            })}
+     </ul>
     )
 }
+
+
 export default ExpensesList
